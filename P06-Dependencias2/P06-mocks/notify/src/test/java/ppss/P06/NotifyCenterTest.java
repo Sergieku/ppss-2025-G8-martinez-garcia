9d8b565ed4feea3f5794436c2fd1e1ae12f18ed7
Sergio Martínez García <smg146@alu.ua.es>
2025-03-20 14:05:01 +0100

    Ejercicio 3

--- P06-Dependencias2/P06-mocks/notify/src/test/java/ppss/P06/NotifyCenterTest.java
+++ P06-Dependencias2/P06-mocks/notify/src/test/java/ppss/P06/NotifyCenterTest.java
@@ -4,6 +4,7 @@ import org.easymock.EasyMock;
 import org.easymock.IMocksControl;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class NotifyCenterTest {
-    LocalDate actualDate, sentDate;
+    LocalDate actualDate, date;
     IMocksControl crtl;
     MailServer server;
     NotifyCenter notifyCentermock;
@@ -32,7 +33,7 @@ public class NotifyCenterTest {
     @Test
     void C1_notifyUsers_should_return_failures_when_2_emails_fail(){
         actualDate = LocalDate.of(2025,03,11);
-        sentDate = actualDate;
+        date = actualDate;
         String[] emails = new String[]{"email1","email2","email3","email4"};
         List<String> emailList = Arrays.asList(emails);
 
@@ -42,7 +43,7 @@ public class NotifyCenterTest {
                 .andReturn(server);
         EasyMock.expect(notifyCentermock.getLocalDate())
                 .andReturn(actualDate);
-        EasyMock.expect(server.findMailItemsWithDate(sentDate))
+        EasyMock.expect(server.findMailItemsWithDate(date))
                         .andReturn(emailList);
 
         // Seguramente eli no lo quiere así, pero a saber cómo lo quiere
@@ -59,9 +60,53 @@ public class NotifyCenterTest {
         crtl.replay();
 
         FailedNotifyException exception = assertThrows(FailedNotifyException.class,
-                () -> notifyCentermock.notifyUsers(actualDate));
+                () -> notifyCentermock.notifyUsers(date));
 
         assertEquals(messageExceptExpected, exception.getMessage());
+
+        crtl.verify();
+    }
+
+    @Test
+    void C2_notifyUsers_should_return_DateError_when_date_passed_not_actualdate(){
+        actualDate = LocalDate.of(2025,03,11);
+        date = LocalDate.of(2025,02,11);
+
+        messageExceptExpected = "Date error";
+
+        EasyMock.expect(notifyCentermock.getServer())
+                .andReturn(server);
+        EasyMock.expect(notifyCentermock.getLocalDate())
+                .andReturn(actualDate);
+
+        crtl.replay();
+
+        FailedNotifyException exception = assertThrows(FailedNotifyException.class,
+                () -> notifyCentermock.notifyUsers(date));
+
+        assertEquals(messageExceptExpected, exception.getMessage());
+
+        crtl.verify();
+    }
+
+    @Test
+    void C3_notifyUsers_should_return_DateError_when_date_correct_and_no_mail(){
+        actualDate = LocalDate.of(2025,03,23);
+        date = actualDate;
+
+        List<String> emailList = new ArrayList<>();
+
+        EasyMock.expect(notifyCentermock.getServer())
+                .andReturn(server);
+        EasyMock.expect(notifyCentermock.getLocalDate())
+                .andReturn(actualDate);
+        EasyMock.expect(server.findMailItemsWithDate(date))
+                .andReturn(emailList);
+
+        crtl.replay();
+
+        assertDoesNotThrow(
+                () -> notifyCentermock.notifyUsers(date));
 
         crtl.verify();
     }
